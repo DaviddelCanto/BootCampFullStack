826ec3d5adb74844824eb6fc7f9ca05074b7c6d3
DaviddelCanto <daviddelcanto@hotmail.com>
2022-11-09 13:44:29 +0100

    a

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import Mensaje from './Mensaje.js'
+
+
+const Descripcion = () => {
+  return <p>Esta es la app para el curso fullstack</p>
+}
 
 function App() {
+
   return (
     <div className="App">
-      Hola Mundo
+      <Mensaje color='red' message='Estamos trabajando'/>
+      <Mensaje color='blue' message='En un curso'/>
+      <Mensaje color='green' message='De React'/>
+      <Descripcion />
     </div>
   );
 }
