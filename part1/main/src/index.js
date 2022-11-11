106af494cf658fd0455ca3598254c0e03612474c
DaviddelCanto <daviddelcanto@hotmail.com>
2022-11-11 07:39:56 +0100

    a

--- part1/main/src/index.js
+++ part1/main/src/index.js
@@ -2,84 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { useState } from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const App = (props) =>{
-
-  const [contador,setContador] = useState(13)
-  /* Version no simplificada de useState
-  const contador = useState(0)
-  const contadorValue =contador[0]
-  const updateContador = contador[1]
-  */
-  const handleClick = () =>{
-    setContador(contador+1)
+const App = () => {
+  // const [left, setLeft] = useState(0)
+  // const [right, setRight] = useState(0)
+  const [counters,setCounters] = useState({
+    left:0,
+    right:0,
+    clicks:0
+  })
+  const handleClickLeft = () =>{
+    const newCounterState ={
+      left : counters.left+1,
+      right : counters.right,
+      clicks : counters.clicks+1
     }
-      const handleClickReset = () =>{
-       setContador(0)
-    }
-
-  const isEven = contador%2===0
-  const mensajePar = isEven ? 'Es par' : 'Es impar'
-
-  console.log('renderizado');
+    setCounters(newCounterState)
+  }
+  const handleClickRight = () =>{
+    setCounters({
+      left : counters.left,
+      right : counters.right+1,
+      clicks : counters.clicks+1
+    })
+  }
   return (
     <div>
-      <p>El valor del contador es: </p>
-      <h1>{contador}</h1>
-      <p>{mensajePar}</p>
-      <button onClick={handleClick}>Incrementar</button>  
-      <button onClick={handleClickReset}>Reset</button>
+      {counters.left}
+      <button onClick={handleClickLeft}>left</button>
+      <button onClick={(handleClickRight)}>right</button>
+      {counters.right}
+      <br></br>
+      <p>Clicks totales: {counters.clicks}</p>
     </div>
   )
 }
 
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-
-
-
-  /*  El boton primeras versiones
-  <button 
-        onClick={handleClick}   //ojo no poner handleClick()
-        // onClick={()=>{
-          //console.log("click")
-          //setContador(contador+1)
-          //Manera optima para saber valor anterior del contador o lo que sea
-          // setContador(prevContador =>{
-          //   return prevContador+1
-          // })
-        // }}
-      >
-  */
-
-
-
-/* Version incorrecta de refrescar la pagina
-
-const App = (props) =>{
-  return <h1>{props.contadorInicial}</h1>
-}
-
-let contador = 0
-
-const refresh = () =>{
-  root.render(
-    <React.StrictMode>
-      <App contadorInicial={contador}/>
-    </React.StrictMode>
-  );
-}
-
-setInterval(() =>{
-  refresh()
-  contador++
-},1000)
-
-*/
-
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
